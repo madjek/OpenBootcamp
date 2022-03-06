f9896056be562421c15bcda08e3d0987d1d38d52
madjek <90720831+madjek@users.noreply.github.com>
2022-03-06 11:53:19 +0100

    Initial commit

---
+++ src/EjT1y2.java
@@ -0,0 +1,17 @@
+public class EjT1y2 {
+
+    public static void main(String[] args) {
+        // Declarar una variable de tipo int llamada numero1.
+        int numero1 = 0;
+        // Mostrarla por pantalla.
+        System.out.println(numero1);
+        // Modificar el valor de la variable numero1.
+        numero1 = numero1 + 10;
+        // Mostrarla por pantalla.
+        System.out.println(numero1);
+        // Crea un array de 4 elementos.
+        int[] array = { 2, 4, 5, 9 };
+        // Mostrar el elemento en la posición 0.
+        System.out.println(array[0]);
+    }
+}
