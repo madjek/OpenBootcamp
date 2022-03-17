ec4e56d0dfddc1dc5460fde9ac501d700e649f54
madjek <90720831+madjek@users.noreply.github.com>
2022-03-17 13:26:22 +0100

    Java Basico Ejercicios tema 1

---
+++ src/com/example/JavaBEjT1.java
@@ -0,0 +1,32 @@
+package com.example;
+
+public class JavaBEjT1 {
+    public static void main(String[] args){
+        //Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+        //Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las
+        // sesiones.
+
+        // 1. numericos
+
+        // 1.1 enteros
+        byte variable1 = 5;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+        long numeroTetefono = 666555444;
+
+        // 1.2 decimales
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+        variable6 = 20.5d;
+        double precioSilla = 49.99;
+
+        // 2. booleano
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        // 3. texto
+        char variabte9 = 'a';
+        String variable1O = "Lorem ipsum dolor sit amet.....";
+    }
+}
