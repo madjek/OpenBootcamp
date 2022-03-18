57d606bcc466a2d7507c0e9b61a0caa0bfa48332
madjek <90720831+madjek@users.noreply.github.com>
2022-03-18 10:45:36 +0100

    Java Basico Ejercicios tema 3

---
+++ JavaBasico/src/com/example/JavaBEjT3.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class JavaBEjT3 {
+
+    public static void main(String[] args){
+        String[] nombres = { "Lucas", "Martín", "Daniel", "Pablo", "Mateo" };
+        for(int i = 0; i < nombres.length; i++){
+            System.out.println(nombres[i]);
+        }
+    }
+}
